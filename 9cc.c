be024a8195ab0cb3db7f032958841103ef92017f
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2020-09-22 18:46:31 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,30 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("    mov rax, %d\n", atoi(argv[1]));
-  printf("    ret\n");
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return EXIT_FAILURE;
+  }
+
+  printf("  ret\n");
   return EXIT_SUCCESS;
 }
--- test.sh
+++ test.sh
@@ -18,6 +18,7 @@ assert() {
 
 assert 0 0
 assert 42 42
+assert 21 "5+20-4"
 
 echo OK""""""""""""""""
 
