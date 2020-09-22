3192593ad4bcd5236709470ccc0cf2f11bdeb130
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2020-09-22 18:19:54 +0900

    整数1つをコンパイルするコンパイラを作成

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+assert() {
+    expected="$1"
+    input="$2"
+
+    ./9cc "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK""""""""""""""""
+
