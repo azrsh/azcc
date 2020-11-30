8f603f1e453876aa0a17ebf1451d5b7c830ec2d2
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2020-11-30 19:07:21 +0900

    ダミーのstring.hを修正

--- test/self/dummylib/string.h
+++ test/self/dummylib/string.h
@@ -3,7 +3,10 @@
 
 #include <stdlib.h>
 
+#define NULL 0
+
 size_t strlen(const char *s);
 void *memcpy(void *dest, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
