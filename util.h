514c274eaa9d68bfb3ae48fae6340b50f8683f3f
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2020-12-05 19:27:36 +0900

    fixup! error関数、error_at関数に_Noreturnを追加

--- util.h
+++ util.h
@@ -7,14 +7,14 @@ bool start_with(const char *p, const char *q);
 
 //エラーを報告するための関数
 // printfと同じ引数をとる
-void _Noreturn error(const char *fmt, ...);
+_Noreturn void error(const char *fmt, ...);
 
 extern const char *user_input;
 extern const char *filename; // 入力ファイル名
 
 //エラーを報告するための関数
 // printfと同じ引数に加えてエラーの位置をとる
-void _Noreturn error_at(const char *location, const char *fmt, ...);
+_Noreturn void error_at(const char *location, const char *fmt, ...);
 
 const char *read_file(const char *path);
 
