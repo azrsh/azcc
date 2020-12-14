7a7f896c2a2871e00d38dd12aaa5eca584c96538
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2020-12-15 01:47:36 +0900

    関数の宣言及び定義のパースをワンパス化

--- src/functioncontainer.h
+++ src/functioncontainer.h
@@ -2,21 +2,15 @@
 #define FUNCTIONCONTAINER
 
 #include "container.h"
+#include "parse.h"
 #include "type.h"
 #include "variable.h"
 
-typedef struct FunctionDeclaration FunctionDeclaration;
-struct FunctionDeclaration {
-  const String *name;
-  Type *returnType;
-  Vector *arguments; // Type vector
-};
-
 typedef struct FunctionContainer FunctionContainer;
 FunctionContainer *new_function_container();
-FunctionDeclaration *function_container_get(FunctionContainer *container,
-                                            const String *name);
+FunctionDefinition *function_container_get(FunctionContainer *container,
+                                           const String *name);
 bool function_container_push(FunctionContainer *container,
-                             FunctionDeclaration *declaration);
+                             FunctionDefinition *declaration);
 
 #endif
