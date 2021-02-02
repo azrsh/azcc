aa939277ea9bb1fe9819f7e56aa865a55c81a9de
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-02-02 14:00:07 +0900

    yanorei32のテストを修正

--- test/yanorei32/fizzbuzz.sh
+++ test/yanorei32/fizzbuzz.sh
@@ -9,56 +9,15 @@ if [ $# != 1 ]; then
 fi
 
 AZCC=$1
-
 TEMP=$(mktemp)
+SRC=`dirname $0`/fizzbuzz.c
 
 set -e
-echo "
-extern int putchar(int c);
-char buff[10];
 
-void itoa_r(int i, char *s) {
-  do {
-    *(s++) = ('0' + (i % 10));
-  } while ((i = i / 10) != 0);
-  *s = 0;
-}
-
-void putstr_r(char *s) {
-  if (!*s) return;
-  putstr_r(s + 1);
-  putchar(*s);
-}
-
-void fizzbuzz(int i) {
-  int printed = 0;
-
-  if (i)
-    fizzbuzz(i - 1);
-
-  if (!(i % 3) && ++printed)
-    putstr_r(\"zziF\");
-  if (!(i % 5) && ++printed)
-    putstr_r(\"zzuB\");
-
-  if (!printed) {
-    itoa_r(i, buff);
-    putstr_r(buff);
-  }
-
-  putchar('\\\\n');
-}
-
-int main() {
-  fizzbuzz(20);
-  return 0;
-}
-" > $TEMP.c
-
-gcc $TEMP.c -o $TEMP.gcc.out
+gcc $SRC -o $TEMP.gcc.out
 $TEMP.gcc.out > $TEMP.gcc.txt
 
-$AZCC $TEMP.c > $TEMP.azcc.s;
+$AZCC $SRC > $TEMP.azcc.s;
 
 gcc $TEMP.azcc.s -o $TEMP.azcc.out -static
 $TEMP.azcc.out > $TEMP.azcc.txt
